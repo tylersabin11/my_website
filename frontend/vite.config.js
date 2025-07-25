1be3fc0d404f8676878321f697436ea32b3cfaf1
Tyler Sabin <124528677+tylersabin11@users.noreply.github.com>
2025-07-25 17:03:19 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,27 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { viteExternalsPlugin } from 'vite-plugin-externals';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  root: './frontend',
   plugins: [
     react(),
     viteExternalsPlugin({
       'react-github-calendar': 'ReactGithubCalendar'
     })
-  ]
+  ],
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './frontend/src'),
+    },
+  },
 });
